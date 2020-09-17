1da0229e20b950b65e950abfd1258d44f1d86d75
tsungtingdu <tsungtingdu@gmail.com>
2020-09-17 13:53:51 +0800

    Updates

---
+++ precache-manifest.2ad81d1e6ab7981d42d21bf434080d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afb2b5b6f12603b196915a1ad94f310",
+    "url": "/ironman-tracker/index.html"
+  },
+  {
+    "revision": "5ee9466e522dba027dab",
+    "url": "/ironman-tracker/static/css/main.939b5ff8.chunk.css"
+  },
+  {
+    "revision": "cd245e29089fa31f5676",
+    "url": "/ironman-tracker/static/js/2.6de26936.chunk.js"
+  },
+  {
+    "revision": "618c50fba1de8680d01b7c244bbe2ad1",
+    "url": "/ironman-tracker/static/js/2.6de26936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee9466e522dba027dab",
+    "url": "/ironman-tracker/static/js/main.ed29fbfa.chunk.js"
+  },
+  {
+    "revision": "c0d53da2ef5df57898b7",
+    "url": "/ironman-tracker/static/js/runtime-main.83a19e06.js"
+  }
+]);
